7438085876478a2dabb9fe64ea2a10c5237428e6
Ameer Hamza <ameer.hamza0046@gmail.com>
2023-10-22 17:29:01 +0530

    added static to express, added app.js, skeleton is ready

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import path from "path";
 const app = express();
+const __dirname = path.resolve();
+
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve() + "/public/index.html");
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(3000, () => {
