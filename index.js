0c777b961d40587789c957be6bec63e521a0705c
Ameer Hamza <ameer.hamza0046@gmail.com>
2023-10-22 12:47:35 +0530

    added index.html in public folder, update index.js to res.sendFile(index.html)

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from "express";
+import path from "path";
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello world... me is here... how is you?... no thank you... you are potato... yayyyy");
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve() + "/public/index.html");
+});
 
 app.listen(3000, () => {
-    console.log("listening...");
-})
+  console.log("listening...");
+});
