0ea039144b6c29ebad43bd0745cdc3f15cc0b633
Ameer Hamza <ameer.hamza0046@gmail.com>
2023-11-04 13:06:35 +0530

    mazeSkeleton method completed

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@ import createBoard from "./components/createBoard.js";
 
 createBoard();
 
-import getInfo from "./components/getInfo.js";
+import mazes from './components/mazes.js';
 
-document.getElementById("btn1").addEventListener("click",() => console.log(getInfo.getStart()));
-document.getElementById("btn2").addEventListener("click",() => console.log(getInfo.getGoal()));
+document.getElementById("btn1").addEventListener("click", mazes.mazeSkeleton);
+document.getElementById("btn2").addEventListener("click",() => console.log("hello"));
--- public/components/mazes.js
+++ public/components/mazes.js
@@ -1,4 +1,39 @@
-
+import getInfo from './getInfo.js'
+const M = getInfo.getM();
+const N = getInfo.getN();
+const getCell = getInfo.getCell;
+//
+const mazeSkeleton = () => {
+    // creating a grid
+    const grid = []
+    for(let i=0; i<M; i++) {
+        grid[i] = Array(N).fill(0);
+    }
+    // filling adjacent cells with walls
+    for(let i=0; i<M; i++) {
+        for(let j=0; j<N; j++) {
+            if(i%2==0 || j%2==0) {
+                grid[i][j] = 1;
+            }
+        }
+    }
+    // copying the skeleton to the board
+    for(let i=0; i<M; i++) {
+        for(let j=0; j<N; j++) {
+            if(grid[i][j]===1) {
+                getCell([i,j]).classList.add("wall");
+            }
+        }
+    }
+}
+//
 const dfsmaze = () => {
 
 }
+
+const mazes = {
+    mazeSkeleton: mazeSkeleton,
+    dfsmaze: dfsmaze,
+}
+
+export default mazes;
