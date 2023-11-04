1f9ccf44df0063ed9de94ffd7cde32c327c94125
Ameer Hamza <ameer.hamza0046@gmail.com>
2023-11-04 20:50:07 +0530

    maze using dfs complete, added new file util.js for simple functions like shuffling an array and rand funtion

--- public/app.js
+++ public/app.js
@@ -4,5 +4,5 @@ createBoard();
 
 import mazes from './components/mazes.js';
 
-document.getElementById("btn1").addEventListener("click", mazes.mazeSkeleton);
+document.getElementById("btn1").addEventListener("click", mazes.dfsmaze);
 document.getElementById("btn2").addEventListener("click",() => console.log("hello"));
---
+++ public/components/util.js
@@ -0,0 +1,18 @@
+// range [0, floor(a))
+function rand(a) {
+    return Math.floor(Math.random()*Math.floor(a));
+}
+
+function shuffleArray(arr) {
+    for(let i=arr.length-1; i>0; i--) {
+        const j = Math.floor(Math.random()*(i+1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+}
+
+const util = {
+    rand: rand,
+    shuffleArray: shuffleArray,
+}
+
+export default util;
